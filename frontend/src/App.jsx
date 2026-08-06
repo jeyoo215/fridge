24488ecb165095a016f1051ddde20f3b55b4bda0
jeyoo215 <je0215yu@gmail.com>
2026-08-06 11:27:51 +0900

    feat: 레시피 상세 화면 구현

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,8 @@
-import { useState } from "react";
-import IngredientList from "./pages/IngredientList";
-import IngredientRegisterForm from "./pages/IngredientRegisterForm";
+import RecipeRecommend from "./pages/RecipeRecommend";
 import "./App.css";
 
-// TODO: 화면이 더 늘어나면 react-router로 교체하기.
-// 지금은 "목록 <-> 등록" 두 화면만 있어서 상태값으로 간단히 전환함.
 function App() {
-  const [view, setView] = useState("list"); // "list" | "register"
-  const [refreshKey, setRefreshKey] = useState(0); // 등록 완료 후 목록 다시 불러오기용
-
-  if (view === "register") {
-    return (
-      <IngredientRegisterForm
-        onCancel={() => setView("list")}
-        onRegistered={() => {
-          setRefreshKey((k) => k + 1);
-          setView("list");
-        }}
-      />
-    );
-  }
-
-  return <IngredientList key={refreshKey} onAddClick={() => setView("register")} />;
+  return <RecipeRecommend />;
 }
 
 export default App;
